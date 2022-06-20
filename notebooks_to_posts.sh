a0b3600a1dc6e723755e8a5e05136b5a193dcd16
Jérome Eertmans <jeertmans@icloud.com>
2022-06-20 11:53:41 +0200

    Script to generate posts from notebooks

---
+++ notebooks_to_posts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+[ -d _notebooks ] && [ "$(find _notebooks -type f -iname '*.ipynb')" ] && python3 -m jupyter nbconvert _notebooks/*.ipynb --to markdown --output-dir assets/images || echo "No notebook found"
+find assets/images -type f -iname '*.md' | xargs -n 1 sed -i -E "s/(!\[svg\])\((.*)\)/\1(\/assets\/images\/\2)/"
+mv assets/images/*.md _posts/
