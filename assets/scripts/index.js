fd24fe4d36f07d0151b54acbc60a1986354f3350
Jérome Eertmans <jeertmans@icloud.com>
2022-06-22 19:35:49 +0200

    Update copy button to look nicer

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,6 @@
-const copyToClipboardButtonStrings = {
-    default: 'Copy',
-    ariaLabel: 'Copy to clipboard',
-    copied: 'Copied',
-};
+const successColor = '#28a745';
+const copyHTML = '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy js-clipboard-copy-icon m-2"><path fill="#fff" d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25v-7.5z"></path><path fill="#fff" d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25v-7.5zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25h-7.5z"></path></svg>';
+const copiedHTML = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check js-clipboard-check-icon color-fg-success d-none m-2"><path fill="${successColor}" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>`;
 
 const includeFileCodeBlocks = document.querySelectorAll('[include-file');
 includeFileCodeBlocks.forEach(async (codeBlock) => {
@@ -27,7 +25,7 @@ copyableCodeBlocks.forEach((codeBlock) => {
     copyButton.className = 'copy';
     copyButton.type = 'button';
     copyButton.ariaLabel = 'Copy code to clipboard';
-    copyButton.innerText = 'Copy';
+    copyButton.innerHTML = copyHTML;
     
     codeBlock.append(copyButton);
 
@@ -35,11 +33,13 @@ copyableCodeBlocks.forEach((codeBlock) => {
         var code = codeBlock.querySelector('code').innerText.trim();
         window.navigator.clipboard.writeText(code);
 
-        copyButton.innerText = 'Copied';
-        var fourSeconds = 4000;
+        copyButton.innerHTML = copiedHTML;
+        copyButton.style.borderColor = successColor;
+        var twoSeconds = 2000;
 
         setTimeout(function () {
-            copyButton.innerText = 'Copy';
-        }, fourSeconds);
+            copyButton.innerHTML = copyHTML;
+            copyButton.style.borderColor = '#fff';
+        }, twoSeconds);
     });
 });
