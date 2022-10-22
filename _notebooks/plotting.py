6c1c9b1da3324096e74b68b3714f5f780abbb689
Jérome Eertmans <jeertmans@icloud.com>
2022-10-22 15:14:34 +0200

    Update plotting.py

--- _notebooks/plotting.py
+++ _notebooks/plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as pyplot
 
 from matplotlib_inline.backend_inline import set_matplotlib_formats
 
-matplotlib.rcParameter["axes.facecolor"] = (1, 1, 1, 0)
+matplotlib.rcParams["axes.facecolor"] = (1, 1, 1, 0)
 set_matplotlib_formats("svg")
 
 __all__ = [
