088302441a7b6ce476d66ca56c2d422acbc36beb
Jérome Eertmans <jeertmans@icloud.com>
2022-10-22 16:24:58 +0200

    fix color

--- _notebooks/plotting.py
+++ _notebooks/plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as pyplot
 
 from matplotlib_inline.backend_inline import set_matplotlib_formats
 
-matplotlib.rcParams["axes.facecolor"] = (1, 0, 0, 0.5)
+matplotlib.rcParams["axes.facecolor"] = (1, 1, 1, 0)  # transparent background
 set_matplotlib_formats("svg")
 
 __all__ = [
