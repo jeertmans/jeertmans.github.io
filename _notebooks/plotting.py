a39c2b53f28fd0313fc11b59231cca1d89abd388
Jérome Eertmans <jeertmans@icloud.com>
2022-10-23 17:24:17 +0200

    fix svg problem

--- _notebooks/plotting.py
+++ _notebooks/plotting.py
@@ -1,6 +1,10 @@
+import matplotlib
 import matplotlib.pyplot as pyplot
 from matplotlib_inline.backend_inline import set_matplotlib_formats
 
+matplotlib.rcParams["figure.facecolor"] = (1, 1, 1, 0)
+matplotlib.rcParams["figure.edgecolor"] = (1, 1, 1, 0)
+matplotlib.rcParams["axes.facecolor"] = (1, 1, 1, 0)
 set_matplotlib_formats("svg")
 
 __all__ = ["pyplot"]
