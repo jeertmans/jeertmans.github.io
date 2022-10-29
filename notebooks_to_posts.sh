011a2771da92f7d119ca46b8939da20a4ca3f229
Jérome Eertmans <jeertmans@icloud.com>
2022-10-29 14:50:43 +0200

    feat: add "go to source file" button

--- notebooks_to_posts.sh
+++ notebooks_to_posts.sh
@@ -2,4 +2,5 @@
 
 [ -d _notebooks ] && [ "$(find _notebooks -type f -iname '*.ipynb')" ] && python3 -m jupyter nbconvert _notebooks/*.ipynb --ExecutePreprocessor.kernel_name=python3 --execute --to markdown --output-dir assets/images || echo "No notebook found"
 find assets/images -type f -iname '*.md' | xargs -n 1 sed -i -E "s/(!\[svg\])\((.*)\)/\1(\/assets\/images\/\2)/"
+find assets/images -type f -iname '*.md' | xargs -n 1 python3 -c "import sys,os;file=sys.argv[1];lines=open(file).readlines();index=lines.index('---\n');basename=os.path.basename(file);path=os.path.join('_notebooks', basename[:-2] + 'ipynb\n');lines.insert(index + 1, f'source: {path}');open(file, 'w').writelines(lines);"
 mv assets/images/*.md _posts/
