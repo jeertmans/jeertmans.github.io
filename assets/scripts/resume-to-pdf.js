87cd6da0e5d79b2ecca67891b9f336fae5f68d45
Jérome Eertmans <jeertmans@icloud.com>
2022-10-30 22:55:50 +0100

    wip: resume online

---
+++ assets/scripts/resume-to-pdf.js
@@ -0,0 +1,10 @@
+import { jsPDF } from  "jsPDF";
+
+var doc = new jsPDF();          
+
+var source = window.document.getElementsByClassName("body")[0];
+gt
+doc.fromHTML(
+    source,
+);
+doc.output("dataurlnewwindow");
