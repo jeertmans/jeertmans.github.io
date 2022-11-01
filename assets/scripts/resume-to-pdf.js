62ebfe5c5040af18163c5482b764c8d0f383e03a
Jérome Eertmans <jeertmans@icloud.com>
2022-11-01 17:12:00 +0100

    wip: printing options

--- assets/scripts/resume-to-pdf.js
+++ assets/scripts/resume-to-pdf.js
@@ -1,8 +1,15 @@
-import { jsPDF } from "jsPDF";
+function converHTMLFileToPDF() {
+	var doc = new jspdf.jsPDF('p','pt','a4');
 
-var doc = new jsPDF();
+	var source = window.document.getElementsByClassName("wrapper")[1];
+    var width = doc.internal.pageSize.getWidth();
+    //var source = window.document.getElementsByTagName("body")[0];
 
-var source = window.document.getElementsByClassName("body")[0];
-gt;
-doc.fromHTML(source);
-doc.output("dataurlnewwindow");
+	// Convert HTML to PDF in JavaScript
+	doc.html(source, {
+		callback: function(doc) {
+			doc.output("dataurlnewwindow");
+		},
+        width: width,
+	});
+}
