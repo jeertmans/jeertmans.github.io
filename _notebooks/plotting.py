ef94ad44745b034660bb9d8ad15e555298ccac43
Jérome Eertmans <jeertmans@icloud.com>
2022-11-05 15:33:15 +0100

    feat: Plotly demo

--- _notebooks/plotting.py
+++ _notebooks/plotting.py
@@ -1,10 +1,31 @@
+import os
+from uuid import uuid4
+
 import matplotlib
 import matplotlib.pyplot as pyplot
+import plotly.express as express
+import plotly.io as pio
+from plotly.graph_objs import Layout, Figure
 from matplotlib_inline.backend_inline import set_matplotlib_formats
+
+iframe_renderer = pio.renderers["iframe_connected"]
+iframe_renderer.html_directory = os.path.join("../assets/images", str(uuid4()))
+pio.renderers.default = "iframe_connected"
+
+layout = Layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    #plot_bgcolor='rgba(0,0,0,0)'
+)
+
+fig = Figure(layout = layout)
+
+templated_fig = pio.to_templated(fig)
+pio.templates['my_template'] = templated_fig.layout.template
+pio.templates.default = 'my_template'
 
 matplotlib.rcParams["figure.facecolor"] = (1, 1, 1, 0)
 matplotlib.rcParams["figure.edgecolor"] = (1, 1, 1, 0)
 matplotlib.rcParams["axes.facecolor"] = (1, 1, 1, 0)
 set_matplotlib_formats("svg")
 
-__all__ = ["pyplot"]
+__all__ = ["express", "pyplot"]
--- notebooks_to_posts.sh
+++ notebooks_to_posts.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-[ -d _notebooks ] && [ "$(find _notebooks -type f -iname '*.ipynb')" ] && python3 -m jupyter nbconvert _notebooks/*.ipynb --ExecutePreprocessor.kernel_name=python3 --execute --to markdown --output-dir assets/images || echo "No notebook found"
-find assets/images -type f -iname '*.md' | xargs -n 1 sed -i -E "s/(!\[svg\])\((.*)\)/\1(\/assets\/images\/\2)/"
-find assets/images -type f -iname '*.md' | xargs -n 1 python3 -c "import sys,os;file=sys.argv[1];lines=open(file).readlines();index=lines.index('---\n');basename=os.path.basename(file);path=os.path.join('_notebooks', basename[:-2] + 'ipynb\n');lines.insert(index + 1, f'source: {path}');open(file, 'w').writelines(lines);"
-mv assets/images/*.md _posts/
+#export PLOTLY_RENDERER=notebook
+
+echo "Executing notebooks..." && \
+    [ -d _notebooks ] && \
+    [ "$(find _notebooks -type f -iname '*.ipynb')" ] && \
+    python3 -m jupyter nbconvert _notebooks/*.ipynb \
+        --ExecutePreprocessor.kernel_name=python3 \
+        --execute \
+        --to markdown \
+        --output-dir assets/images || \
+    echo "No notebook found"
+
+echo "Changing link to svg files" && \
+    find assets/images -type f -iname '*.md' | \
+    xargs -n 1 sed -i -E "s/(!\[svg\])\((.*)\)/\1(\/assets\/images\/\2)/"
+
+echo "Changing link to html image files" && \
+    find assets/images -type f -iname '*.md' | \
+    xargs -n 1 sed -i -E 's/src="\.\.\//src="\//'
+
+echo "Changing relative path for posts generated from notebooks" && \
+    find assets/images -type f -iname '*.md' | \
+    xargs -n 1 python3 -c "import sys,os;file=sys.argv[1];lines=open(file).readlines();index=lines.index('---\n');basename=os.path.basename(file);path=os.path.join('_notebooks', basename[:-2] + 'ipynb\n');lines.insert(index + 1, f'source: {path}');open(file, 'w').writelines(lines);"
+
+echo "Moving new posts to _posts directory" && \
+    mv assets/images/*.md _posts/
