289e329f18a8fa1fb6f2d3ced26784ec43999191
Jérome Eertmans <jeertmans@icloud.com>
2022-11-05 17:06:39 +0100

    feat: enhanced plot

--- _notebooks/plotting.py
+++ _notebooks/plotting.py
@@ -5,27 +5,34 @@ import matplotlib
 import matplotlib.pyplot as pyplot
 import plotly.express as express
 import plotly.io as pio
-from plotly.graph_objs import Layout, Figure
 from matplotlib_inline.backend_inline import set_matplotlib_formats
+from plotly.graph_objs import Figure, Layout
 
 iframe_renderer = pio.renderers["iframe_connected"]
 iframe_renderer.html_directory = os.path.join("../assets/images", str(uuid4()))
 pio.renderers.default = "iframe_connected"
 
-layout = Layout(
-    paper_bgcolor='rgba(0,0,0,0)',
-    #plot_bgcolor='rgba(0,0,0,0)'
-)
+CONFIG = {"displaylogo": False, "toImageButtonOptions": {"format": "svg"}}
 
-fig = Figure(layout = layout)
 
-templated_fig = pio.to_templated(fig)
-pio.templates['my_template'] = templated_fig.layout.template
-pio.templates.default = 'my_template'
+for theme, template in [("light", "plotly_white"), ("dark", "plotly_dark")]:
+
+    fig = Figure()
+    fig.update_layout(
+        template=template,
+        paper_bgcolor="rgba(0,0,0,0)",
+        plot_bgcolor="rgba(0,0,0,0)",
+        font_color="#d37e34",
+    )
+
+    templated_fig = pio.to_templated(fig)
+    pio.templates[theme] = templated_fig.layout.template
+
+pio.templates.default = "light"
 
 matplotlib.rcParams["figure.facecolor"] = (1, 1, 1, 0)
 matplotlib.rcParams["figure.edgecolor"] = (1, 1, 1, 0)
 matplotlib.rcParams["axes.facecolor"] = (1, 1, 1, 0)
 set_matplotlib_formats("svg")
 
-__all__ = ["express", "pyplot"]
+__all__ = ["express", "pyplot", "CONFIG"]
