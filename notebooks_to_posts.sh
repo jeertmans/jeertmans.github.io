73b7f13c4c5a18fdb556397b3946d662b7a3b103
Jérome Eertmans <jeertmans@icloud.com>
2023-02-26 21:56:56 +0100

    fix(ci): move notebooks assets to new dir

--- notebooks_to_posts.sh
+++ notebooks_to_posts.sh
@@ -9,20 +9,20 @@ echo "Executing notebooks..." && \
         --ExecutePreprocessor.kernel_name=python3 \
         --execute \
         --to markdown \
-        --output-dir assets/images || \
+        --output-dir assets/notebooks || \
     echo "No notebook found"
 
 echo "Changing link to svg files" && \
-    find assets/images -type f -iname '*.md' | \
-    xargs -n 1 sed -i -E "s/(!\[svg\])\((.*)\)/\1(\/assets\/images\/\2)/"
+    find assets/notebooks -type f -iname '*.md' | \
+    xargs -n 1 sed -i -E "s/(!\[svg\])\((.*)\)/\1(\/assets\/notebooks\/\2)/"
 
 echo "Changing link to html image files" && \
-    find assets/images -type f -iname '*.md' | \
+    find assets/notebooks -type f -iname '*.md' | \
     xargs -n 1 sed -i -E 's/src="\.\.\//src="\//'
 
 echo "Changing relative path for posts generated from notebooks" && \
-    find assets/images -type f -iname '*.md' | \
+    find assets/notebooks -type f -iname '*.md' | \
     xargs -n 1 python3 -c "import sys,os;file=sys.argv[1];lines=open(file).readlines();index=lines.index('---\n');basename=os.path.basename(file);path=os.path.join('_notebooks', basename[:-2] + 'ipynb\n');lines.insert(index + 1, f'source: {path}');open(file, 'w').writelines(lines);"
 
 echo "Moving new posts to _posts directory" && \
-    mv assets/images/*.md _posts/
+    mv assets/notebooks/*.md _posts/
