29a779444881a3480bc838e228f5c468529052ed
Jérome Eertmans <jeertmans@icloud.com>
2023-08-31 17:04:25 +0200

    clear old assets

--- _slides/make_slides.sh
+++ _slides/make_slides.sh
@@ -1,2 +1,3 @@
+rm -rf ../../assets/slides/
 find -iname "main.py" | xargs -I {} bash -c 'cd $(dirname {}) && manim "$(basename {})" --media_dir="../media/$(dirname {})" && manim-slides convert Main "../../assets/slides/$(dirname {}).html"'
 
