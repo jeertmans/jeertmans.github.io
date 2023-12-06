7be57034b03e9d47ab7ec55b8197d6d1e9155ca9
Jérome Eertmans <jeertmans@icloud.com>
2023-12-06 10:27:26 +0100

    add dummy axes

--- _slides/2023-12-07-confirmation/main.py
+++ _slides/2023-12-07-confirmation/main.py
@@ -822,6 +822,43 @@ class Main(Slide, MovingCameraScene):
     def construct_drt(self):
         self.next_slide(notes="Differentiable Ray Tracing part!")
         self.new_clean_slide("Differentiable Ray Tracing")
+        grid = Axes(
+            x_range=[0, 1, 0.05],  # step size determines num_decimal_places.
+            y_range=[0, 1, 0.05],
+            x_length=9,
+            y_length=5.5,
+            axis_config={
+                "numbers_to_include": np.arange(0, 1 + 0.1, 0.1),
+                "font_size": 24,
+            },
+            tips=False,
+        )
+
+        # Labels for the x-axis and y-axis.
+        y_label = grid.get_y_axis_label("y", edge=LEFT, direction=LEFT, buff=0.4)
+        x_label = grid.get_x_axis_label("x")
+        grid_labels = VGroup(x_label, y_label)
+
+        graphs = VGroup()
+        for n in np.arange(1, 20 + 0.5, 0.5):
+            graphs += grid.plot(lambda x: x ** n, color=WHITE)
+            graphs += grid.plot(
+                lambda x: x ** (1 / n), color=WHITE, use_smoothing=False
+            )
+
+        # Extra lines and labels for point (1,1)
+        graphs += grid.get_horizontal_line(grid.c2p(1, 1, 0), color=BLUE)
+        graphs += grid.get_vertical_line(grid.c2p(1, 1, 0), color=BLUE)
+        graphs += Dot(point=grid.c2p(1, 1, 0), color=YELLOW)
+        graphs += Tex("(1,1)").scale(0.75).next_to(grid.c2p(1, 1, 0))
+        title = Title(
+            # spaces between braces to prevent SyntaxError
+            r"Graphs of $y=x^{ {1}\over{n} }$ and $y=x^n (n=1,2,3,...,20)$",
+            include_underline=False,
+            font_size=40,
+        )
+        self.add(title, graphs, grid, grid_labels)
+        self.wait()
 
     def construct_status_of_work(self):
         self.next_slide(notes="Now, we will take a look at the status of work.")
@@ -925,10 +962,10 @@ class Main(Slide, MovingCameraScene):
     def construct(self):
         self.wait_time_between_slides = 0.10
 
-        self.construct_intro()
-        self.construct_fundamentals()
-        self.construct_motivations()
-        self.construct_tracing()
+        #self.construct_intro()
+        #self.construct_fundamentals()
+        #self.construct_motivations()
+        #self.construct_tracing()
         self.construct_drt()
-        self.construct_status_of_work()
+        #self.construct_status_of_work()
         self.construct_conclusion()
