9bc93880086981448f2318a683a9dc2e7c0c3e70
Jérome Eertmans <jeertmans@icloud.com>
2025-01-21 17:15:19 +0100

    wip(slides): render scene with Plotly

--- _slides/2025-01-27-dublin-presentation/main.py
+++ _slides/2025-01-27-dublin-presentation/main.py
@@ -1,10 +1,13 @@
 import os
+import io
 
 import jax.numpy as jnp
+from PIL import Image
 import numpy as np
 from manim import *
 from manim_slides import ThreeDSlide
 from differt.scene import TriangleScene, download_sionna_scenes, get_sionna_scene
+import differt.plotting as dplt
 from plotly.colors import convert_to_RGB_255
 
 # Constants
@@ -43,6 +46,7 @@ Text.set_default(color=BLACK, font_size=CONTENT_FONT_SIZE)
 download_sionna_scenes()
 scene = TriangleScene.load_xml(get_sionna_scene("simple_street_canyon"))
 
+dplt.set_defaults("plotly")
 
 def paragraph(*strs, alignment=LEFT, direction=DOWN, **kwargs):
     texts = VGroup(*[Text(s, **kwargs) for s in strs]).arrange(direction)
@@ -52,43 +56,6 @@ def paragraph(*strs, alignment=LEFT, direction=DOWN, **kwargs):
             text.align_to(texts[0], direction=alignment)
 
     return texts
-
-class Plane(ThreeDVMobject):
-    def __init__(self, vertices, **kwargs):
-        super().__init__(shade_in_3d=True,**kwargs)
-        self.set_points_as_corners([*vertices, vertices[-1]])
-
-def scene_to_mobjects(scene, prism=True):
-    objects = []
-    scale = 0.05
-    for i, j in scene.mesh.object_bounds:
-        sub_mesh = scene.mesh[i:j]
-
-        bbox = sub_mesh.bounding_box.T * scale
-
-        dx, dy, dz = jnp.diff(bbox, axis=1)
-        xavg, yavg, zavg = jnp.mean(bbox, axis=1)
-
-        color = rgb_to_color(sub_mesh.face_colors[0, :].tolist())
-
-
-        if prism:
-            p = Prism([dx, dy, dz], fill_color=color).move_to([xavg, yavg, zavg])
-        else:
-            p = Polyhedron((scale * sub_mesh.vertices).tolist(),sub_mesh.triangles.tolist(), faces_config=dict(color=color, fill_color=color, fill_opacity=1))
-
-        if zavg < 1e-6:
-            zavg = -0.1
-            (xmin, xmax), (ymin, ymax) = bbox[:2,:].tolist()
-            p = Plane([[xmin, ymin, zavg], [xmax, ymin, zavg], [xmax, ymax, zavg], [xmin, ymax, zavg]], color=color, fill_color=color, fill_opacity=.9, z_index=-1)
-            #p = Surface(lambda x,y: (x,y,-0.01), u_range=[xmin, xmax], v_range=[ymin, ymax], fill_color=color, checkerboard_colors=False, stroke_color=color, z_index=-1)
-            #p = Rectangle(width=xmax-xmin, height=ymax-ymin, color=color, fill_color=color, fill_opacity=1, z_index=-1).move_to([xavg, yavg, -0.5])
-        
-        objects.append(
-        p
-        )
-
-    return objects
 
 class Main(ThreeDSlide):
 
@@ -100,9 +67,18 @@ class Main(ThreeDSlide):
         self.set_camera_orientation(phi=60 * DEGREES, theta=150 * DEGREES)
         #self.set_camera_orientation(phi=0* DEGREES, theta=150 * DEGREES)
 
-        prims = scene_to_mobjects(scene)
+        fig = scene.plot()
 
-        self.add(*prims)
+        img_bytes = fig.to_image(format="png", scale=2)
+        img_pil = Image.open(io.BytesIO(img_bytes))
+
+        print(f"{img_bytes = }")
+
+        im = ImageMobject(img_pil)
+
+        print(f"{im = }")
+
+        self.add(im)
 
         self.next_slide(loop=True)
         self.begin_ambient_camera_rotation(90*DEGREES, about='theta')
